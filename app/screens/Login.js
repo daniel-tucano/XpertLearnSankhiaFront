d752f6b9970dafdf4be573b8c4a72ef5943785f4
rafaelsant <rafael.ribeiro.santiago@gmail.com>
2020-12-29 00:53:42 -0300

    alterações de estilos na tela de login

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,68 +1,99 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
-import { TextInput } from "react-native-gesture-handler";
-import { Fontisto, Ionicons } from "@expo/vector-icons";
+import React from 'react'
+import { StyleSheet, View, Text, Image } from 'react-native'
+import {
+    TextInput,
+    TouchableHighlight,
+    TouchableOpacity,
+} from 'react-native-gesture-handler'
+import { Fontisto, Ionicons } from '@expo/vector-icons'
 
 function Login(props) {
-  return (
-    <View style={styles.container}>
-      <View style={styles.text}>
-        <Text></Text>
-      </View>
-      <View style={styles.login}>
-        <Ionicons name="person" size={24} color="white" />
-        <TextInput
-          style={styles.textInput}
-          textContentType="emailAddress"
-          editable
-          placeholder="Email"
-        />
-      </View>
-      <View style={styles.login}>
-        <Fontisto name="locked" size={24} color="white" />
-        <TextInput
-          style={styles.textInput}
-          textContentType="password"
-          editable
-          placeholder="senha"
-        />
-      </View>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.login}>
+                <Image style={styles.logo} />
+            </View>
+            <View style={styles.login}>
+                <Ionicons name="person" size={24} color="#026B2E" />
+                <TextInput
+                    style={styles.textInput}
+                    textContentType="emailAddress"
+                    editable
+                    placeholder="Email"
+                />
+            </View>
+            <View style={styles.login}>
+                <Fontisto name="locked" size={24} color="#026B2E" />
+                <TextInput
+                    style={styles.textInput}
+                    textContentType="password"
+                    editable
+                    placeholderTextColor="grey"
+                    placeholder="Senha"
+                />
+            </View>
+            <View style={styles.login}>
+                <TouchableOpacity style={styles.loginButton}>
+                    <Text style={styles.loginButtonText}>Login</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#026B2E",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  login: {
-    height: 38,
-    width: "70%",
-    position: "relative",
-    bottom: 50,
-    margin: 10,
-    borderRadius: 10,
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  text: {
-    height: 50,
-    width: "70%",
-    position: "relative",
-    bottom: 50,
-    margin: 10,
-    borderRadius: 10,
-  },
-  textInput: {
-    marginLeft: 8,
-    width: "90%",
-    height: "100%",
-    borderBottomWidth: 1,
-    borderBottomColor: "white",
-    borderBottomEndRadius: 50,
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#E5E3E3',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    login: {
+        height: 38,
+        width: '70%',
+        position: 'relative',
+        bottom: 50,
+        margin: 10,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        height: 50,
+        width: '70%',
+        position: 'relative',
+        bottom: 50,
+        margin: 10,
+        borderRadius: 10,
+    },
+    textInput: {
+        marginLeft: 8,
+        width: '90%',
+        height: '100%',
+        borderBottomWidth: 1,
+        borderBottomColor: '#026B2E',
+        borderBottomEndRadius: 50,
+    },
+    loginButton: {
+        alignItems: 'center',
+        backgroundColor: '#026B2E',
+        width: 200,
+        height: 40,
+        justifyContent: 'center',
+        borderRadius: 5,
+        shadowColor: 'black',
+        shadowOpacity: 3,
+        elevation: 8,
+        shadowRadius: 15,
+        shadowOffset: { width: 56, height: 13 },
+    },
+    loginButtonText: {
+        color: 'white',
+    },
+    logo: {
+        height: 80,
+        width: 80,
+    },
+})
 
-export default Login;
+export default Login
