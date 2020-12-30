d5697ee5784cd2b955dea8cd31c825dd09f6da3c
rafaelsant <rafael.ribeiro.santiago@gmail.com>
2020-12-30 04:12:38 -0300

    adicionado botão voltar

--- app/components/PostImageView.jsx
+++ app/components/PostImageView.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { View,StyleSheet } from 'react-native';
 import { ImageViewer } from 'react-native-image-zoom-viewer'
+import { Ionicons } from '@expo/vector-icons'; 
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 function PostImageView(props) {
     return (
         <View style={styles.container}>
+            <TouchableOpacity>
+                <Ionicons 
+                style={styles.button} 
+                name="arrow-back-circle-outline" 
+                size={50} 
+                color="white" />
+            </TouchableOpacity>
             <ImageViewer imageUrls={props.images} renderIndicator={()=>null} style={styles.image}/>
         </View>
     );
@@ -16,6 +25,8 @@ const styles = StyleSheet.create({
     },
     image:{
         flex:1
+    },button:{
+        justifyContent:'flex-start'
     }
 })
 
